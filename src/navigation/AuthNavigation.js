26b8c71ce1be71afc809567dcbbe76f70bb610ba
Carlos Diaz <carlos12one@hotmail.com>
2023-05-30 22:17:14 -0500

    add screen

--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthScreen } from "../screens/Auth";
+import { AuthScreen, LoginEmailScreen, RegisterEmailScreen } from "../screens/Auth";
 import { useTheme } from "../hooks";
-import { getNavigationTheme } from "../utils";
+import { getNavigationTheme, screen } from "../utils";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +12,16 @@ const AuthNavigation = () => {
   const MyTheme = getNavigationTheme(theme);
   return (
     <NavigationContainer theme={MyTheme}>
-      <Stack.Navigator>
-        <Stack.Screen name="auth" component={AuthScreen} />
+      <Stack.Navigator screenOptions={{ headerTransparent: true }}>
+        <Stack.Screen
+          name={screen.auth.auth}
+          component={AuthScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name={screen.auth.loginEmail} component={LoginEmailScreen} />
+        <Stack.Screen name={screen.auth.registerEmail} component={RegisterEmailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
